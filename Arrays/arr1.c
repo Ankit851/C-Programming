10372f0855c34bb67de3d9a716c0ecf6b6f8d924
Ankit Mahajan <69541646+Ankit851@users.noreply.github.com>
2021-07-04 14:49:56 +0530

    Create arr1.c

---
+++ Arrays/arr1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+    for(int i = 0 ; i < 5 ; i++ )
+    {
+        printf("The value at the index location %d is %d\n",i,arr[i]);
+    }
+    
+    /*for(int j=0; j < 5 ; j++)
+    {
+        scanf("%d",&arr[j]);
+    }
+    */
+}
